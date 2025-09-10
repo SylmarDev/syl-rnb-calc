b1b896e4ab82ff89869517c97b6cd70b285b6f27
Sylvia Oleander-Rothove <sylmarssbm@gmail.com>
2025-09-10 18:13:25 -0500

    1.3.1 Message

--- src/js/data/changelog.js
+++ src/js/data/changelog.js
@@ -24,5 +24,6 @@ const CHANGELOG = [
     new ChangelogLine(1, 2, 7, "Leech Seed no longer clicked against grass types"),
     new ChangelogLine(1, 2, 8, "Leech Seed no longer clicked against grass types for real oops"),
     new ChangelogLine(1, 2, 9, "Parasol Lady Madeline and Camper Lawrence (Shed Shell double) show as one fight"),
-    new ChangelogLine(1, 3, 0, "Search bar searches by trainer name as well, added Toggleable Box search feature, toggles stay selected in between sessions")
+    new ChangelogLine(1, 3, 0, "Search bar searches by trainer name as well, added Toggleable Box search feature, toggles stay selected in between sessions"),
+    new ChangelogLine(1, 3, 1, "Grassy Glide now gets +1 Prio when it AI sees fast kill in Grassy Terrain, Self-Destruct procs AI Options to show up (thanks dylanrae)")
 ];
